5a88ff7dc302240560667d7046a627c4cb54bc00
OTHMANE RAHEL <rahelothmane@gmail.com>
2025-01-12 19:45:50 +0100

    Projet modifie par othmane

--- Othmane tasks/Controller/EtudiantController.php
+++ Othmane tasks/Controller/EtudiantController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CoursesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,13 +21,34 @@ class EtudiantController extends AbstractController
     public function questionsCours(CoursesRepository $course, $id): Response
     {
         $courses = $course->findByIdCourse($id);
-        // dd($courses);
         return $this->render('etudiant/quizQuestion.html.twig', ["courses" => $courses]);
     }
-    #[Route("/note-etudiant",name:"acceuil.note")]
-    public function traitementNote(Request $request){
-        dd($request);
-        return $this->render("etudiant/interfaceNote.html.twig");
+    #[Route("/note-etudiant",name:"acceuil.note",methods:["POST","GET"])]
+    public function traitementNote(Request $request,CoursesRepository $course) : Response{
+        $note = 0;
+        $reponses = $request->request->all();
+        dd($reponses["reponse_Qu'est-ce_que_SQL_?"]);
+        $idCourse = $reponses["id_course"];
+        $courses = $course->findByIdCourse($idCourse);
+        //dd($courses[0]["reponses"]);
+        //dd($courses);
+        //dd($reponses[$courses[0]["reponse_vrai"]] );
+        // $i=0;
+        // do {
+        //    $note++;
+        //    $i++;
+        // } while ($i <= 10);
+        for($i = 0 ; $i < count($courses) ; $i++){
+            dd($reponses["reponse_Quelle_est_la_syntaxe_pour_une_fonction_en_C++_?"]);
+            // if($courses[$i]["reponse_vrai"] == $reponses[$courses[$i]["reponses"][$i]]){
+            //     $note += 2;
+            // }
+            // $reps = $reponses["reponse_2"];
+        }
+        // foreach($reponses as $r){
+        //     dd($r);
+        // }
+        return $this->render('etudiant/interfaceNote.html.twig', ["rep" => $reponses,"note"=>$note]);
     }
     
 }
--- Repository/CoursesRepository.php
+++ Repository/CoursesRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Courses;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @extends ServiceEntityRepository<Courses>
@@ -45,4 +46,32 @@ class CoursesRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
+public function findByIdCourse($id) {
+    // $entityManager = $this->getEntityManager()->getConnection();
+    // $sql = 'select DISTINCT(q.question) , r.reponse from questions q
+    //     join reponse r on r.question_id = q.id
+    //     join courses c on c.id = q.course_id
+    //     where c.id = :id';
+    // $stmt = $entityManager->executeQuery($sql,['id' => $id]);
+    // return $stmt->fetchAllAssociative();
+    $course = $this->getEntityManager()
+        ->getRepository(Courses::class)
+        ->find($id);
+    $result = [];
+    foreach ($course->getQuestions() as $question) {
+        $reponses = [];
+        foreach ($question->getReponse() as $reponse) {
+            $reponses[] = $reponse->getReponse();
+        }
+
+        $result[] = [
+            'course_id' => $course->getId(),
+            'question_id' => $question->getId(),
+            'question' => $question->getQuestion(),
+            'reponses' => $reponses,
+            'reponse_vrai' => $question->getReponseVrai()
+        ];
+    }
+    return $result;
+}
 }
